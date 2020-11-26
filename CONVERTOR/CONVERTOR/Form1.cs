e0b07827bc4ac763cb9bd8ad74f8c0eb0950f9a3
Ravindi Kumarasinghe <57246252+ravindikumarasinghe@users.noreply.github.com>
2020-11-26 23:20:32 +0530

    Update code

--- CONVERTOR/CONVERTOR/Form1.cs
+++ CONVERTOR/CONVERTOR/Form1.cs
@@ -10,11 +10,7 @@ using System.Windows.Forms;
 
 namespace CONVERTOR
 {
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            InitializeComponent();
+
         }
                   private void button1_Click(object sender, EventArgs e)
                 
