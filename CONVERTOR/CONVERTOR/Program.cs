23982e89613b78da68d3158cb1c07a95b20a1934
Ravindi Kumarasinghe <57246252+ravindikumarasinghe@users.noreply.github.com>
2020-11-26 23:18:31 +0530

    Update code

--- CONVERTOR/CONVERTOR/Program.cs
+++ CONVERTOR/CONVERTOR/Program.cs
@@ -11,12 +11,7 @@ namespace CONVERTOR
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+
         }
     }
 }
