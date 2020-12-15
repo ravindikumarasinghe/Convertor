c20ffcc61e5971936e385b5bf7bb31f9abfe33da
Ravindi Kumarasinghe <57246252+ravindikumarasinghe@users.noreply.github.com>
2020-12-15 23:41:06 +0530

    update code

--- CONVERTOR/CONVERTOR/Form1.cs
+++ CONVERTOR/CONVERTOR/Form1.cs
@@ -10,18 +10,26 @@ using System.Windows.Forms;
 
 namespace CONVERTOR
 {
+  
   public partial class Form1 : Form
   {
     public Form1()
-    {
+   
+   {
       InitializeComponent();
     }
+    
           private void button1_Click(object sender, EventArgs e)
+    
     {
         float number1 = float.Parse(txt1.Text);
+        
         float ans = number1 / 1000;
+        
         txt2.Text = ans.ToString();
-    }      
+        
+    }
+    
   }
   
 }
